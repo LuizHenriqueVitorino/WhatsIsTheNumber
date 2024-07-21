76fc912ef9c6623a1acd0142b95dbff1c08237da
LuizHenriqueVitorino <henriquefisicoquimico@gmail.com>
2024-07-21 10:24:23 -0300

    feature: cria os endpoints de gerar um número, visualizar o número criado e de comparar o número

---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+from random import randint
+
+app = Flask(__name__)
+
+NUMBER = None
+
+@app.route('/generete', methods=['POST'])
+def genereteNumber():
+    global NUMBER
+    number = randint(1, 100)
+    NUMBER = number
+    return jsonify({'code': 200, 'message': 'Número aleatório gerado', 'number': NUMBER}), 200
+
+@app.route('/view', methods=['GET'])
+def viewNumber():
+    return jsonify({'code': 200, 'number': NUMBER}), 200
+
+@app.route('/check', methods=['POST'])
+def checkNumber():
+    data = request.get_json()
+
+    if not data or 'number' not in data:
+        return jsonify({'code': 400, 'message': 'Forneça um número.'}), 400
+    
+    if NUMBER is None:
+        return jsonify({'code': 400, 'message': 'Número não definido.'}), 400
+
+    number = data["number"]
+
+    if not isinstance(number, int):
+        return jsonify({'code': 400, 'message': 'Precisa ser um número inteiro.'}), 400
+
+    if number < NUMBER:
+        message = f'O número {number} é menor'
+
+    if number > NUMBER:
+        message = f'O número {number} é maior'
+
+    if number == NUMBER:
+        message = f'O número {number} está correto'
+
+    return jsonify({'code': 200, 'message': message}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- backend/teste.py
+++ backend/teste.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
 
-app = Flask(__name__)
+def teste(number):
+    if not isinstance(number, int):
+        return 'Precisa ser um número inteiro.'
+    
+    return number
 
-@app.route("/")
-def hello_world():
-    return "<p>Parabéns, vc está acessando o servidor do Luiz Henrique.</p>"
+print(teste(10))
