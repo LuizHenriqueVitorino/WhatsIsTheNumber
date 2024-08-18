e20980124e9e57fd3ad7834e0f1de22c4e4d843d
Lucas Rocha <lr359508@gmail.com>
2024-08-17 21:55:50 -0300

    feat: outros números de endereço adicionados

--- backend/app.py
+++ backend/app.py
@@ -45,4 +45,4 @@ def checkNumber():
     return jsonify({'code': 200, 'message': message}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
